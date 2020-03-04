7d736b8f80199f10bc797f4de84b756a6ee68ae2
Vinay <vjanard@gmail.com>
2020-03-04 21:40:05 +0000

    updated snake.py

--- snake.py
+++ snake.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python
 from samplebase import SampleBase
 import random
-import sys
-import select
+##import keyboard
 import time
-import termios
-import tty
-
-def _getch():
-	fd = sys.stdin.fileno()
-	old_settings = termios.tcgetattr(fd)
-	try:
-		tty.setraw(fd)
-		ch = sys.stdin.read(1)
-	finally:
-		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-	return ch
-
-
 class snake(object):
 
     def __init__(self):
@@ -72,9 +57,26 @@ class snake(object):
         return
 
 
-    def getUserInput(self):
+    '''def getUserInput(self):
+        key = keyboard.read_key()
         
-	char = _get_char()
+        if key == "w":
+            print("W was pressed!")
+            self.snakeYdelta = 1
+            self.snakeXdelta = 0
+        elif key == "a":
+            print("a was pressed!")
+            self.snakeYdelta = 0
+            self.snakeXdelta = -1
+        elif key == "s":
+            print("s was pressed!")
+            self.snakeYdelta = -1
+            self.snakeXdelta = 0
+        elif key == "d":
+            print("d was pressed!")
+            self.snakeYdelta = 0
+            self.snakeXdelta = 1
+        return'''
 
 
     def checkDotEat(self):
@@ -92,49 +94,41 @@ class snake(object):
 
     def getGameScreen(self):
         return self.gameScreen
->>>>>>> Stashed changes
 
 
 class colorMatrix(SampleBase):
 
-	def __init__(self, *args, **kwargs):
-		self.snake = snake()
-		super(colorMatrix, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.snake = snake()
+        super(colorMatrix, self).__init__(*args, **kwargs)
 
 
-	def run(self):
+    def run(self):
 
-		#create virtual matrix
-		
-		offset_canvas = self.matrix.CreateFrameCanvas()
-		tmp_matrix = snake.getGameScreen
+        #create virtual matrix
+        
+        offset_canvas = self.matrix.CreateFrameCanvas()
+        tmp_matrix = snake.getGameScreen
 
-		#forever
-		while True:
-			self.snake.cycle()
-			tmp_matrix = self.snake.getGameScreen()
-			color = 0
+        #forever
+        while True:
+            self.snake.cycle()
+            tmp_matrix = self.snake.getGameScreen()
+            color = 0
 
-			for x in range(0, self.matrix.width):
-				for y in range(0, self.matrix.height):
-					#column, row, red, blue, green
-					color = tmp_matrix[x][y]
-					offset_canvas.SetPixel(x, y, 255*color, 0, 0)
+            for x in range(0, self.matrix.width):
+                for y in range(0, self.matrix.height):
+                    #column, row, red, blue, green
+                    color = tmp_matrix[x][y]
+                    offset_canvas.SetPixel(x, y, 255*color, 0, 0)
 
-<<<<<<< Updated upstream
-			offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
-			print("End of Cycle!")
-=======
             offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+            print("End of Cycle!")
 
 # Main function
 if __name__ == "__main__":
-	#Create colorMatrix
-	mat = colorMatrix()
+    #Create colorMatrix
+    mat = colorMatrix()
 
-	if (not mat.process()):
-		print('broked')
+    if (not mat.process()):
+print('broked')
