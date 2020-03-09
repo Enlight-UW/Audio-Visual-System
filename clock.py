6b5d025f575ec4293a015345f851877d067fd8fb
kkim6109 <kkim434@wisc.edu>
2020-03-09 18:19:14 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -105,14 +105,14 @@ class colorMatrix(SampleBase):
     
     def getTimeMat(self, num, x_offset):
         time_matrix = []
-        for pos in self.getNumber(int(1)): # first num
-            pos[0] += self.offset_y
-            pos[1] += self.offset_x + x_offset
+        for pos in self.getNumber(int(num/10)): # first num
+            pos[0] += 8
+            pos[1] += 8 + x_offset
             time_matrix.append(pos)
                 
-        for pos in self.getNumber(int(1)): # Second num
-            pos[0] += self.offset_x + 6 + x_offset # 6 for the 2nd number
-            pos[1] += self.offset_y
+        for pos in self.getNumber(int(num%10)): # Second num
+            pos[0] += 8 + 6 + x_offset # 6 for the 2nd number
+            pos[1] += 8
             time_matrix.append(pos)
             
         return time_matrix
